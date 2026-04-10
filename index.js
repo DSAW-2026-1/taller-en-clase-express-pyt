abf2a7fbfb042a43eaf6cd8ecda54f533d9cf333
Sebastián Núñez <sebasnunezfoca@gmail.com>
2026-04-10 09:19:33 -0500

    ruta raiz agregada

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('API funcionando 🚀');
+    res.send('API funcionando');
 });
 
 const authRoutes = require('./routes/auth');
